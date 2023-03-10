1cb13fe64ffff1c91a990f8a06c5dd01b2b7d553
Christopher Lupp <christopherlupp@gmail.com>
2023-03-09 22:01:34 -0700

    initial explicit client function implementations

--- Philote/explicit_client.hpp
+++ Philote/explicit_client.hpp
@@ -1,3 +1,10 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <utility>
+#include <Philote/array.hpp>
+#include <Philote/discrete_array.hpp>
 
 /**
  * @brief Client class for calling a remote explicit discipline.
@@ -7,6 +14,7 @@
  */
 class ExplicitClient
 {
+public:
     //! Constructor
     ExplicitClient();
 
@@ -25,8 +33,16 @@ class ExplicitClient
      * Unlike the analysis server, this function does not need to be overridden,
      * as it contains all logic necessary to retrieve the remote function
      * evaluation.
+     *
+     * @param inputs
+     * @param discrete_inputs
+     * @param outputs
+     * @param discrete_outputs
      */
-    void RemoteCompute();
+    void RemoteCompute(std::map<std::string, Array> &inputs,
+                       std::map<std::string, DiscreteArray> &discrete_inputs,
+                       std::map<std::string, Array> &outputs,
+                       std::map<std::string, DiscreteArray> &discrete_outputs);
 
     /**
      * @brief Calls the remote analysis server function evaluation via gRPC.
@@ -34,6 +50,52 @@ class ExplicitClient
      * Unlike the analysis server, this function does not need to be overridden,
      * as it contains all logic necessary to retrieve the remote function
      * evaluation.
+     *
+     * @param inputs
+     * @param discrete_inputs
+     * @param partials
      */
-    void RemoteComputePartials();
+    void RemotePartials(std::map<std::string, Array> &inputs,
+                        std::map<std::string, DiscreteArray> &discrete_inputs,
+                        std::map<std::pair<std::string, std::string>, Array> &partials);
+
+private:
+    //! names of all functions defined for this discipline
+    std::vector<std::string> funcs_;
+
+    //! shape of all functions defined for this discipline
+    std::map<std::string, std::vector<size_t>> funcs_shape_;
+
+    //! units of all functions defined for this discipline
+    std::map<std::string, std::string> funcs_units_;
+
+    //! names of all discrete functions defined for this discipline
+    std::vector<std::string> discrete_funcs_;
+
+    //! shape of all discrete functions defined for this discipline
+    std::map<std::string, std::vector<size_t>> discrete_funcs_shape_;
+
+    //! shape of all discrete functions defined for this discipline
+    std::map<std::string, std::string> discrete_funcs_units_;
+
+    //! names of all variables defined for this discipline
+    std::vector<std::string> vars_;
+
+    //! shape of all variables defined for this discipline
+    std::map<std::string, std::vector<size_t>> vars_shape_;
+
+    //! units of all variables defined for this discipline
+    std::map<std::string, std::string> vars_units_;
+
+    //! names of all discrete variables defined for this discipline
+    std::vector<std::string> discrete_vars_;
+
+    //! shape of all discrete variables defined for this discipline
+    std::map<std::string, std::vector<size_t>> discrete_vars_shape_;
+
+    //! units of all discrete variables defined for this discipline
+    std::map<std::string, std::string> discrete_vars_units_;
+
+    //! vector of all defined partials
+    std::vector<std::pair<std::string, std::string>> partials_;
 };
