3fae859c375d194b38f60fed62ada0517d2dd463
Christopher Lupp <christopherlupp@gmail.com>
2023-03-14 18:53:20 -0400

    modified array class

--- Philote/array.hpp
+++ Philote/array.hpp
@@ -10,14 +10,48 @@
 class Array
 {
 public:
-    //! Constructor
+    //! Default Constructor
     Array();
+
+    //! Default Constructor
+    Array(size_t params, ...);
 
     //! Destructor
     ~Array();
 
+    /**
+     * @brief Assigns a segment of the array given a subvector
+     *
+     * @param start starting index of the segment
+     * @param end ending index of the segment
+     * @param data data to be assigned to the segment
+     */
+    void Segment(size_t &start, size_t &end, std::vector<double> &data);
+
+    /**
+     * @brief Returns the shape of the array
+     *
+     * @return std::vector<size_t> vector containing the length of the
+     * individual dimensions
+     */
+    std::vector<size_t> Shape() const;
+
+    /**
+     * @brief Returns the size of the array.
+     *
+     * @return size_t size of the array
+     */
+    size_t Size() const;
+
     //! Accessor
-    double operator()(size_t params, ...);
+    /**
+     * @brief Returns the value of the array at a given index
+     *
+     * @param params indices
+     * @param ...
+     * @return double value of the array at the given indices
+     */
+    double operator()(size_t params, ...) const;
 
 private:
     //! array shape
