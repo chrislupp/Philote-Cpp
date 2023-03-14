9e88dfc6d4c02bd6a5fc516b35cf4e4eb27f57a4
Christopher Lupp <christopherlupp@gmail.com>
2023-03-14 18:53:07 -0400

    added unit testing using gtest

---
+++ test/array_tests.cpp
@@ -0,0 +1,25 @@
+
+#include <Philote/array.hpp>
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(ArrayTests, Constructor)
+{
+    Array a = Array();
+
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
+
+// Demonstrate some basic assertions.
+TEST(ArrayTests, More)
+{
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
