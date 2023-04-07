8d330c0901fbc5e94b2bc21a3e1ae511e3555a00
Christopher Lupp <christopherlupp@gmail.com>
2023-04-07 16:11:29 -0400

    Feature/ci (#2)
    * changed variadic constructor
    
    * modified operator() for array
    
    * added test for array segment function
    
    * switched to grpc ubuntu package for CI
    
    * added grpc github action
    
    * updated grpc version
    
    * reset grpc version
    
    * added verbose build to CI
    
    * added protoc include
    
    * removed parallel build
    
    * changed protobuf compile
    
    * removed verbose build
    
    * changed working dir for protoc compile
    
    * deactivated other proto files
    
    * switched to absolute dir
    
    * added submodule checkout to actions
    
    * added PAT TOKEN for auth
    
    * temporarily disabled grpc generation
    
    * moved back to group protoc compile
    
    * reactivated grpc gen

--- src/utils/array.cpp
+++ src/utils/array.cpp
@@ -3,35 +3,40 @@
 #include <stdexcept>
 #include <Philote/array.hpp>
 
+#include <iostream>
+
 Array::Array() {}
 
-Array::Array(size_t params, ...)
+Array::Array(const std::vector<size_t> &shape)
 {
-    // get the indices for assignment
-    std::va_list args;
-    va_start(args, params);
-    for (size_t i = 0; i < params; ++i)
-    {
-        shape_.push_back(va_arg(args, size_t));
-    }
-    va_end(args);
+    // assign the array shape
+    shape_ = shape;
+
+    // serialized array size
+    size_t size = 1;
+    for (size_t i = 0; i < shape_.size(); i++)
+        size *= shape_[i];
+
+    // initialize the array from the shape input
+    data_.resize(size);
 }
 
 Array::~Array() {}
 
-void Array::Segment(size_t &start, size_t &end, std::vector<double> &data)
+void Array::Segment(const size_t &start, const size_t &end,
+                    const std::vector<double> &data)
 {
     // check that the segment matches length of (end - start)
-    if ((end - start) != data.size())
+    if ((end - start) + 1 != data.size())
     {
-        std::string expected = std::to_string(end - start);
+        std::string expected = std::to_string((end - start) + 1);
         std::string actual = std::to_string(data.size());
         throw std::length_error("Vector data has incompatable length. Should be " +
                                 expected + ", but received " + actual + ".");
     }
 
     // assign the segment
-    for (size_t i = 0; i < (end - start); i++)
+    for (size_t i = 0; i < (end - start) + 1; i++)
         data_[start + i] = data[i];
 }
 
@@ -45,34 +50,7 @@ size_t Array::Size() const
     return data_.size();
 }
 
-double Array::operator()(size_t params, ...) const
+double Array::operator()(const size_t &i) const
 {
-
-    std::vector<size_t> indices;
-
-    // get the indices for assignment
-    std::va_list args;
-    va_start(args, params);
-    for (size_t i = 0; i < params; ++i)
-    {
-        indices.push_back(va_arg(args, size_t));
-    }
-    va_end(args);
-
-    size_t index = 0;
-    for (size_t i = 0; i < 0; i++)
-    {
-        index += indices[i] * shape_[i];
-    }
-
-    // check to make sure the index is within the length of the array
-    if (index != data_.size())
-    {
-        auto expected = std::to_string(data_.size());
-        auto actual = std::to_string(index);
-        throw std::out_of_range("Index is out of bounds. Should be smaller than " +
-                                expected + ", but received " + actual + ".");
-    }
-
-    return data_[index];
+    return data_[i];
 }
