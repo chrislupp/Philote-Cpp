01b156066838edb2544999c18bd3c065a407eb1c
Christopher Lupp <christopherlupp@gmail.com>
2023-04-07 23:47:01 -0400

    added grpc function definitions for expserver

--- src/general/explicit_server.cpp
+++ src/general/explicit_server.cpp
@@ -1,13 +1,57 @@
 
 #include <Philote/explicit_server.h>
 
+using google::protobuf::Empty;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
 using namespace philote;
 
 ExplicitServer::ExplicitServer() {}
 
 ExplicitServer::~ExplicitServer() {}
 
-void ExplicitServer::Setup() {}
+Status SetStreamOptions(ServerContext *context,
+                        const ::Options *request,
+                        Empty *response)
+{
+    return Status::OK;
+}
+
+Status DefineVariables(ServerContext *context,
+                       const Empty *request,
+                       ServerWriter<::VariableMetaData> *writer)
+{
+    return Status::OK;
+}
+
+Status DefinePartials(ServerContext *context,
+                      const Empty *request,
+                      ServerWriter<::PartialsMetaData> *writer)
+{
+    return Status::OK;
+}
+
+Status Functions(ServerContext *context,
+                 ServerReaderWriter<::Array, ::Array> *stream)
+{
+    return Status::OK;
+}
+
+Status Gradient(ServerContext *context,
+                ServerReaderWriter<::Array, ::Array> *stream)
+{
+    return Status::OK;
+}
+
+// these functions need to be overridden by discipline developers
+//------------------------------------------------------------------------------
+void ExplicitServer::Setup()
+{
+}
 
 void ExplicitServer::SetupPartials() {}
 
