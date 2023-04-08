4c8cf6594b4331683e92268a6dc6da1ce862c294
Christopher Lupp <christopherlupp@gmail.com>
2023-04-07 23:12:11 -0400

    added inheritance to expserver

--- Philote/explicit_server.h
+++ Philote/explicit_server.h
@@ -6,6 +6,8 @@
 #include <Philote/array.h>
 #include <Philote/discrete_array.h>
 
+#include <explicit.grpc.pb.h>
+
 namespace philote
 {
     /**
@@ -14,7 +16,7 @@ namespace philote
      * This class should be inherited from by analysis discipline developers to
      * create analysis servers.
      */
-    class ExplicitServer
+    class ExplicitServer : public ExplicitDiscipline::Service
     {
     public:
         //! Constructor
@@ -27,22 +29,33 @@ namespace philote
          * @brief Sets up the analysis server before any function or gradient
          * evaluation.
          *
-         * This function should be overridden by the developer of the discipline.
+         * This function should be overridden by the developer of the
+         * discipline.
          */
         virtual void Setup();
 
         /**
+         * @brief Defines the partials for this discipline.
+         *
+         * This function should be overridden by the developer of the
+         * discipline.
+         */
+        virtual void SetupPartials();
+
+        /**
          * @brief Function evaluation for the discipline.
          *
-         * This function should be overridden by the developer of the discipline.
+         * This function should be overridden by the developer of the
+         * discipline.
          */
         virtual void Compute();
 
         /**
          * @brief Function evaluation for the discipline.
          *
-         * This function should be overridden by the developer of the discipline, if
-         * applicable (not every discipline can provide partials).
+         * This function should be overridden by the developer of the
+         * discipline, if applicable (not every discipline can provide
+         * partials).
          */
         virtual void ComputePartials();
 
--- src/general/explicit_server.cpp
+++ src/general/explicit_server.cpp
@@ -9,6 +9,8 @@ ExplicitServer::~ExplicitServer() {}
 
 void ExplicitServer::Setup() {}
 
+void ExplicitServer::SetupPartials() {}
+
 void ExplicitServer::Compute() {}
 
 void ExplicitServer::ComputePartials() {}
