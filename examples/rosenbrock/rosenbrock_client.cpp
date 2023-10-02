4699c4df2589939e4eb58441db40c513545f4c8c
Christopher Lupp <christopherlupp@gmail.com>
2023-10-01 23:52:54 -0400

    started rosenbrock client

--- examples/rosenbrock/rosenbrock_client.cpp
+++ examples/rosenbrock/rosenbrock_client.cpp
@@ -15,9 +15,121 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+#include <iostream>
+
+#include <grpcpp/grpcpp.h>
+
+#include <Philote/variable.h>
+#include <Philote/explicit.h>
+
+using std::cout;
+using std::endl;
+using std::make_pair;
+using std::string;
+using std::vector;
+
+using grpc::Channel;
+
+using philote::DisciplineOptions;
+using philote::Partials;
+using philote::Variable;
+using philote::Variables;
 
 
-int main()
+int main(int argc, char *argv[])
 {
+	if (argc != 2) {
+		std::cerr << "Usage: " << argv[0] << " <number>" << std::endl;
+		return 1;
+	}
+
+	// Read the command line argument as a string
+	std::string input = argv[1];
+	char *endptr;
+	unsigned long n = std::strtoll(input.c_str(), &endptr, 10);
+
+
+	// create the gRPC channel
+	std::shared_ptr<Channel> channel = grpc::CreateChannel("localhost:50051",
+			grpc::InsecureChannelCredentials());
+	philote::ExplicitClient client;
+	client.ConnectChannel(channel);
+
+	// send stream options to the analysis server
+	client.SendStreamOptions();
+
+	// create and send discipline options
+	philote::DisciplineOptions options;
+	client.SendOptions(options);
+
+	// send stream options to the analysis server
+	client.Setup();
+
+	// get the variable meta data from the server
+	client.GetVariableDefinitions();
+
+	vector<string> vars = client.GetVariableNames();
+	cout << "Variable List" << endl;
+	for (auto &name : vars)
+	{
+		cout << name << endl;
+	}
+	cout << endl
+		 << endl;
+
+	for (auto &name : vars)
+	{
+		auto var = client.GetVariableMeta(name);
+		cout << "name: " << var.name() << ", ";
+
+		if (var.type() == philote::kInput)
+			cout << "input";
+		else if (var.type() == philote::kOutput)
+		{
+			cout << "output";
+		}
+		cout << endl;
+	}
+
+	// get the partials meta data from the server
+	client.GetPartialDefinitions();
+
+	auto partials_meta = client.partials_meta();
+
+	cout << endl;
+	cout << "Partials Meta" << endl
+		 << "-------------" << endl;
+	for (const auto &par : partials_meta)
+	{
+		cout << "d" << par.name() << "/d" << par.subname() << ", size: " << par.shape()[0] << endl;
+	}
+
+	// define the inputs and run a function evaluation
+	Variables inputs;
+	inputs["x"] = Variable(philote::kInput, {n});
+
+	inputs["x"](0) = 1.0;
+
+	Variables outputs = client.ComputeFunction(inputs);
+
+	cout << endl
+		 << endl;
+	cout << "Outputs" << endl;
+	cout << "-------" << endl;
+	for (auto &var : outputs)
+	{
+		cout << "name: " << var.first << ", " << var.second(0) << endl;
+	}
+
+	// run a gradient evaluation
+	Partials partials = client.ComputeGradient(inputs);
+
+	cout << endl
+		 << endl
+		 << "Partials" << endl
+		 << "--------" << endl;
+	cout << "df_dx: " << partials[make_pair("f_xy", "x")](0) << endl;
+	cout << "df_dy: " << partials[make_pair("f_xy", "y")](0) << endl;
+
     return 0;
 }
